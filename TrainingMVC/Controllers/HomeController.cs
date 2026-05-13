4e974d2e26de6330341279ea092214e635a032eb
Leena Majed <leenamajed8@gmail.com>
2026-05-14 01:13:54 +0300

    adding about,training and contact pages.

--- TrainingMVC/Controllers/HomeController.cs
+++ TrainingMVC/Controllers/HomeController.cs
@@ -22,7 +22,18 @@ namespace TrainingMVC.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Training()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
